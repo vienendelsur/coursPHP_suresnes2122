b13abff5d896be0326f83a303b69851f13dd3c46
Patrick Isola <isola.patrick@gmail.com>
2021-12-10 14:35:22 +0100

    début cours

---
+++ 01-intro/01_introduction.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>CoursPHP - Suresnes 2021/2022</title>
+    <!-- mes styles -->
+    <link rel="stylesheet" href="../css/style.css">
+    
+  </head>
+  <body>
+    <header class="container-fluid bg-light">
+      <h1 class="display-4">CoursPHP - Introduction</h1>
+      <p class="lead">PHP : Php Hypertext Preprocessor</p>
+    </header>
+
+    <div class="container bg-white">
+        <section class="row m-2 p-2">
+          <div class="col-md-4 p-1 border border-primary">
+            <h4>1/ Réaliser un site dynamique</h4>
+            <p>Pour réaliser une site dynamique nous allons aborder les points suivants : </p>
+            <ul>
+              <li>La syntaxe et les caractéristiques du langage PHP (v7)</li>
+              <li>Les notions essentielles du langage SQL permettant la gestion d'une BDD et la réalisation des requêtes de base</li>
+              <li>... et les moyens d'y accéder à l'aide de fonctions spécialisées de PHP (ou d'objet)</li>
+            </ul>
+          </div>
+          <!-- fin col -->
+
+          <div class="col-md-4 p-1 border border-warning">
+            <h4>2/ Qu'est-ce que PHP </h4>
+          </div>
+          <!-- fin col -->
+
+          <div class="col-md-4 p-1 border border-danger">
+          <h4>3/ Rappel sur les BDD </h4>
+          </div>
+          <!-- fin col -->
+        </section>
+        <!-- fin row  -->
+    </div>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    
+  </body>
+</html>
+
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# coursPHP_suresnes2122
+# coursPHP_suresnes2122 
 initiation et cours PHP Colombbus
+
+
+DOSSIERS
+## 00-exos
+## 00-pages
+    ### 01_ma_page_php.php
+## 01-intro
+    ### 01_introduction.php
+        #### 1/ Réaliser un site dynamique 
+        #### 2/ PHP
+    ### infos.php
+## 02-variables
+    ### 01_variables.php
+    ### 02_types.php
+    ### 03_chaines.php
+    ### 04_tableaux.php
+## 03-conditions
+    ### 01_conditions.php
+    ### 02_boucles.php
+## 04-get
+    ### 01_method_get.php
+    ### 02_method_get.php
+## 05-post
+    ### 01_method_post.php
+## 06-pdo
+    ### 01_pdo.php
+## 07-cookies
+    ### 01_cookies.php
+## 08-session
+    ### 01_session.php
+    ### 02_session.php
+    ### 03_session.php
+## 09-securite
+    ### 01_dialogue.php
+    ### 02_employes.php
+    ### 03_employes_suite.php
+    ### 04_fiche_employe.php
+## inc (inc = include)
+    ### functions.php
+    ### head.inc.php
+    ### body.inc.php
+    ### nav.inc.php
+    ### footer.inc.php
+
+## 10-boutique
+ SOMMAIRE À COMPLÉTER
+
+## css
+## img
+## js
