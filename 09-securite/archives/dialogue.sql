b4e1998f6c446443e9d64f859f54f208373d2451
Patrick Isola <isola.patrick@gmail.com>
2022-01-07 09:45:10 +0100

    sauvegarde BDD

---
+++ 09-securite/archives/dialogue.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:8889
+-- Généré le :  ven. 07 jan. 2022 à 08:42
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `dialogue`
+--
+CREATE DATABASE IF NOT EXISTS `dialogue` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `dialogue`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaires`
+--
+
+CREATE TABLE `commentaires` (
+  `id_commentaires` int(3) NOT NULL,
+  `pseudo` varchar(20) NOT NULL,
+  `message` text NOT NULL,
+  `date_enregistrement` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `commentaires`
+--
+
+INSERT INTO `commentaires` (`id_commentaires`, `pseudo`, `message`, `date_enregistrement`) VALUES
+(24, 'test', 'Coucou les amis', '2021-03-22 20:04:26'),
+(25, 'test', '¿ Hola qué tal? ', '2021-03-22 20:04:31'),
+(26, 'riri', 'riri est mon ami\r\n', '2021-03-23 00:00:00'),
+(27, 'Lorem', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2022-01-07 00:00:00'),
+(28, 'Dupont', 'Ceci est un commentaire', '2022-01-07 00:00:00');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  ADD PRIMARY KEY (`id_commentaires`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  MODIFY `id_commentaires` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
